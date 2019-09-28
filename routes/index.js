77de703ee0819aea2826c7b67d4b512eb67d984c
Quinton Fults <quinton.fults.work@gmail.com>
2019-09-28 10:38:32 -0700

    simplify data

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 module.exports = (app, spotify) => {
   app.get('/search', (req, res) => {
-    spotify.search({ type: req.query.t ? req.query.t : 'track', query: req.query.q ? req.query.q : 'Dancing Queen' }, (err, data) => {
+    spotify.search({ type: req.query.t ? req.query.t : 'track', query: req.query.q ? req.query.q : 'Dancing Queen' }, (err, { tracks: { items } }) => {
       if (err) {
         res.json(err)
       }
-      res.json(data)
+      res.json(items)
     })
   })
 }
