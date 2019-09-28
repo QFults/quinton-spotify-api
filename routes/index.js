301890ae1a2fc78e54fe2ba3040c9965d75b6d5b
Quinton Fults <quinton.fults.work@gmail.com>
2019-09-28 11:14:50 -0700

    fixed artist & album break

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 module.exports = (app, spotify) => {
   app.get('/search', (req, res) => {
-    spotify.search({ type: req.query.t ? req.query.t : 'track', query: req.query.q ? req.query.q : 'Dancing Queen' }, (err, { tracks: { items } }) => {
+    spotify.search({ type: req.query.t && 'trackartistalbum'.includes(req.query.t) ? req.query.t : 'track', query: req.query.q ? req.query.q : 'Dancing Queen' }, (err, data) => {
       if (err) {
         res.json(err)
-      }  
-      spotify.request(`https://api.spotify.com/v1/audio-features?ids=${items.map(({ id }) => id).join(',')}`, (err, { audio_features }) => {
-        if (err) {
-          res.json(err)
-        }
-        res.json(items.map((song, i) => ({ ...song, ...audio_features[i]})))
-      })
+      }
+      if (data.tracks) {
+        const items = data.tracks.items
+        spotify.request(`https://api.spotify.com/v1/audio-features?ids=${items.map(({ id }) => id).join(',')}`, (err, { audio_features }) => {
+          if (err) {
+            res.json(err)
+          }          
+          res.json(items.map((song, i) => ({ ...song, ...audio_features[i] })))
+        })
+      } else if (data.artists) {
+        res.json(data.artists.items)
+      } else {
+        res.json(data.albums.items)
+      }
     })
   })
 
