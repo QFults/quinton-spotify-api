ecf789cef42a0920392c8b56b9c5032206899745
Quinton Fults <quinton.fults.work@gmail.com>
2019-09-28 10:06:01 -0700

    added edge case coverage

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = (app, spotify) => {
   app.get('/search', (req, res) => {
-    spotify.search({ type: req.query.t, query: req.query.q }, (err, data) => {
+    spotify.search({ type: req.query.t ? req.query.t : 'track', query: req.query.q ? req.query.q : 'Dancing Queen' }, (err, data) => {
       if (err) {
         res.json(err)
       }
